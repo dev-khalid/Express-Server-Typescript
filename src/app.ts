6f7a80e5db27d82ec05db10a50d4990a2a340ff3
dev-khalid <khalid.hossain.akash@gmail.com>
2023-01-07 10:54:06 +0600

    Socket implementation

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,22 @@
 import express, { Application } from "express";
-import { Server } from "node:http";
+import { createServer } from "node:http";
+import cors from "cors";
+import { Server } from "socket.io";
 const app: Application = express();
-interface Todo {
-  x: string;
-}
-const y: Todo = { x: "1" };
-const json = '{"x":"5","y":"str"}';
-const data: { x: number } = JSON.parse(json); //object literal doesn't know who is y so it is refusing it.
-const profile = {
-  age: 20,
-  name: "akash",
-};
-const { age }: { age: number; name: string } = profile;
-
-interface Vehicle {
-  name: string;
-  year: string;
-  summary(): string;
-}
-const oldCivic = {
-  name: "toyota",
-  year: "10-12-2020",
-  summary(): string {
-    return `${this.name} & ${this.year}`;
+const httpServer = createServer(app);
+app.use(cors());
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
   },
-};
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(vehicle.summary());
-};
-class Vehicle {
-  protected honk(): void {
-    console.log("Vroom!");
-  }
-}
-class Car extends Vehicle {
-  private someMethod(): void {
-    this.honk();
-  }
-  private color: string = "red";
-  private data: any;
-}
-const car = new Car();
-app.listen(5000, () => {
+});
+let arr = ["1", "2", "3", "4", "5"];
+io.on("connection", (socket) => {
+  console.log(`⚡Connected id: ${socket.id}`);
+  let someData = { name: "akash" }; 
+    socket.emit(arr[0], someData); 
+});
+
+httpServer.listen(5000, () => {
   console.log("Server running on port : 5000");
 });
